8043d301deca6315639fb22884db8a1ce0021369
Stepan Hakobyan <stepan.hakobyan@live.com>
2018-11-30 15:13:30 +0400

    to non unicode, hmm

--- assets/manageThemes.js
+++ assets/manageThemes.js
@@ -1,30 +1,30 @@
 let csstag = document.getElementById("darktheme");
 if (document.cookie.indexOf("theme:dark") >= 0) {
-  csstag.rel = "stylesheet";
+    csstag.rel = "stylesheet";
 } else {
-  csstag.rel = "";
+    csstag.rel = "";
 }
 
 window.addEventListener("load", () => {
-  let goDark = document.createElement("a");
-  goDark.href = "#";
-  goDark.classList.add("page-link");
-  goDark.style.filter = "grayscale(100%)";
-  if (document.cookie.indexOf("theme:dark") >= 0) {
-    goDark.addEventListener("click", () => { 
-      document.cookie = "theme:light";
-      document.location.reload();
-    });
-    goDark.textContent = "☀";
-    goDark.title = "Լուսավոր";
-  } else {
-    goDark.addEventListener("click", () => {
-      document.cookie = "theme:dark";
-      document.location.reload();
-    });
-    goDark.textContent = "🌙";
-    goDark.title = "Խավար";
-  }
-  let nav = document.querySelector(".site-nav .trigger");
-  nav.appendChild(goDark);
+    let goDark = document.createElement("a");
+    goDark.href = "#";
+    goDark.classList.add("page-link");
+    goDark.style.filter = "grayscale(100%)";
+    if (document.cookie.indexOf("theme:dark") >= 0) {
+        goDark.addEventListener("click", () => {
+            document.cookie = "theme:light";
+            document.location.reload();
+        });
+        goDark.innerHTML = "\u{2600}";  //BLACK SUN WITH RAYS
+        goDark.title = "Light";
+    } else {
+        goDark.addEventListener("click", () => {
+            document.cookie = "theme:dark";
+            document.location.reload();
+        });
+        goDark.innerHTML = "\u{1F319}"; //CRESCENT MOON
+        goDark.title = "Dark";
+    }
+    let nav = document.querySelector(".site-nav .trigger");
+    nav.appendChild(goDark);
 });
