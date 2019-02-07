21b49cf1d2137a6a039be80ea85f0fda0ba084c8
Stepan Hakobyan <stepan.hakobyan@live.com>
2019-02-07 14:39:24 +0400

    Theme switch (#1)
    * fix theme button location
    
    * theme switch in js
    
    * theme switch title

--- assets/manageThemes.js
+++ assets/manageThemes.js
@@ -7,26 +7,22 @@ if (window.localStorage) {
     }
 
     window.addEventListener("load", () => {
-        let goDark = document.createElement("a");
-        goDark.href = "#";
-        goDark.classList.add("page-link");
-        goDark.style.filter = "grayscale(100%)";
+        let goDark = document.querySelector(".site-nav .trigger .theme-switch");
+        //goDark.style.filter = "grayscale(100%)";
         if (window.localStorage["theme"] == "dark") {
             goDark.addEventListener("click", () => {
                 window.localStorage["theme"] = "light";
                 document.location.reload();
             });
             goDark.innerHTML = "\u{2600}";  //BLACK SUN WITH RAYS
-            goDark.title = "Light";
+            goDark.title = "Լուսավորել";
         } else {
             goDark.addEventListener("click", () => {
                 window.localStorage["theme"] = "dark";
                 document.location.reload();
             });
-            goDark.innerHTML = "\u{1F319}"; //CRESCENT MOON
-            goDark.title = "Dark";
+            //goDark.innerHTML = "\u{1F319}"; //CRESCENT MOON
+            //goDark.title = "Մթնացնել";
         }
-        let nav = document.querySelector(".site-nav .trigger");
-        nav.appendChild(goDark);
     });
 }
